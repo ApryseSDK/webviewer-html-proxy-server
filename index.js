d210a9a701ceede33dcdbca35e76271f18c2ec63
maggievu <mvu@pdftron.com>
2022-03-23 12:19:08 -0700

    fix server-side request forgery attacks

--- index.js
+++ index.js
@@ -7,17 +7,19 @@ const cookieParser = require('cookie-parser');
 const { URL } = require('url');
 const fs = require('fs');
 const path = require('path');
+const isValidURL = require('./utils/isValidURL.js');
 
-const debounceJS = fs.readFileSync(path.resolve(__dirname, './utils/debounceJS.js'), 'utf8');
-const sendTextDataScript = fs.readFileSync(path.resolve(__dirname, './utils/getTextData.js'), 'utf8');
-const blockNavigationScript = fs.readFileSync(path.resolve(__dirname, './utils/blockNavigation.js'), 'utf8');
-const blockNavigationStyle = fs.readFileSync(path.resolve(__dirname, './utils/blockNavigation.css'), 'utf8');
+const debounceJS = fs.readFileSync(path.resolve(__dirname, './inject-proxy/debounceJS.js'), 'utf8');
+const sendTextDataScript = fs.readFileSync(path.resolve(__dirname, './inject-proxy/getTextData.js'), 'utf8');
+const blockNavigationScript = fs.readFileSync(path.resolve(__dirname, './inject-proxy/blockNavigation.js'), 'utf8');
+const blockNavigationStyle = fs.readFileSync(path.resolve(__dirname, './inject-proxy/blockNavigation.css'), 'utf8');
 
 function createServer({
   SERVER_ROOT,
   PORT,
   CORS_OPTIONS = { origin: `${SERVER_ROOT}:3000`, credentials: true },
-  COOKIE_SETTING = {}
+  COOKIE_SETTING = {},
+  ALLOW_HTTP_PROXY = false,
 }) {
   console.log('createServer', SERVER_ROOT, PORT);
 
@@ -26,11 +28,6 @@ function createServer({
   app.use(cors(CORS_OPTIONS));
 
   const PATH = `${SERVER_ROOT}:${PORT}`;
-
-  const isValidURL = (url) => {
-    // eslint-disable-next-line no-useless-escape
-    return /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi.test(url);
-  }
 
   const getHostPortSSL = (url) => {
     const {
@@ -71,7 +68,7 @@ function createServer({
     // this is the url retrieved from the input
     const url = req.query.url;
     // ****** first check for human readable URL with simple regex
-    if (!isValidURL(url)) {
+    if (!isValidURL(url, ALLOW_HTTP_PROXY)) {
       res.status(400).send({ errorMessage: 'Please enter a valid URL and try again.' });
     } else {
       // ****** second check for puppeteer being able to goto url
---
+++ utils/isValidURL.js
@@ -0,0 +1,61 @@
+const BLOCKLIST = [
+  "127.",
+  "0.",
+  "10.",
+  "172.16.",
+  "172.17.",
+  "172.18.",
+  "172.19.",
+  "172.20.",
+  "172.21.",
+  "172.22.",
+  "172.23.",
+  "172.24.",
+  "172.25.",
+  "172.26.",
+  "172.27.",
+  "172.28.",
+  "172.29.",
+  "172.30.",
+  "172.31.",
+  "192.168.",
+  "169.254.",
+  "fc",
+  "fd",
+  "fe",
+  "ff",
+  "::1",
+]
+
+const isValidURL = (url, allowHTTPProxy) => {
+  let link = url.toLowerCase();
+  const { hostname, port, protocol } = new URL(link);
+
+  if (!allowHTTPProxy) {
+    if (protocol === 'http:') {
+      console.log('http proxy')
+      return false;
+    }
+  }
+
+  // Confirm this is a domain not an IP address by checking the hostname
+  // ends with a two-letter or three-letter domain.
+  if (!(/[a-zA-Z]{2,3}$/.test(hostname))) {
+    return false;
+  }
+
+  // Be suspicious of anything that supplies a port.
+  if (port) {
+    return false;
+  }
+
+  // Check the block list of forbidden sites.
+  if (BLOCKLIST.some(el => hostname.startsWith(el))) {
+    return false;
+  }
+
+  // eslint-disable-next-line no-useless-escape
+  return /(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/gi.test(link);
+}
+
+module.exports = isValidURL;
