808242d5ec243e1965c9bce3cbd0b332d5a58cb5
maggievu <mvu@pdftron.com>
2022-04-04 18:07:05 -0700

    add more CSS/JS rules for blocking navigation

--- inject-proxy/blockNavigation.js
+++ inject-proxy/blockNavigation.js
@@ -8,22 +8,25 @@ const blockNavigation = () => {
   const { urlToProxy } = window.PDFTron;
 
   // block navigation for all a tags that don't start with #  
-  document.querySelectorAll('a:not([href^="#"])').forEach(elem => {
+  document.querySelectorAll('a:not([href]), a[href=""], a[href]:not([href^="#"]):not([href="javascript: void(0)"])').forEach(elem => {
     // in subsequent debouncing, make sure to only run this for new <a>
-    if (!!elem.href && elem.dataset.pdftron != 'pdftron') {
-      elem.setAttribute('target', '_blank');
+    if (elem.dataset.pdftron != 'pdftron') {
       // set this attibute to identify if <a> href has been modified
       elem.setAttribute('data-pdftron', 'pdftron');
-      elem.setAttribute('data-href', elem.getAttribute('href'));
 
-      // If the url is absolute then new URL won't mess it up.
-      // It will only append urlToProxy if it is relative.
-      elem.setAttribute('href', new URL(elem.getAttribute('href'), urlToProxy).href);
-
-      elem.addEventListener('click', (event) => {
-        event.stopImmediatePropagation();
-        event.stopPropagation();
-      });
+      if (!!elem.href) {
+        elem.setAttribute('target', '_blank');
+        elem.setAttribute('data-href', elem.getAttribute('href'));
+        // If the url is absolute then new URL won't mess it up.
+        // It will only append urlToProxy if it is relative.
+        elem.setAttribute('href', new URL(elem.getAttribute('href'), urlToProxy).href);
+        elem.addEventListener('click', (event) => {
+          event.stopImmediatePropagation();
+          event.stopPropagation();
+        });
+      } else if (elem.onclick) {
+        elem.onclick = null;
+      }
     }
   });
 
