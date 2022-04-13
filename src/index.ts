7d34410da0afa2eb9152c5156eb79b4d16faa241
maggievu <mvu@pdftron.com>
2022-04-13 15:50:57 -0700

    add JSDOM and try to replace vh values

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import cookieParser from 'cookie-parser';
 import type { ClientRequest, IncomingMessage } from 'http';
 import type { Request, Response } from 'express';
 import { createLogger, format, transports } from 'winston';
+import { JSDOM } from 'jsdom';
 
 // import from data types
 import type { PageDimensions, ProxyRequestOptions, PuppeteerOptions, ServerConfigurationOptions, Viewport } from './utils/data.js';
@@ -13,6 +14,7 @@ import type { PageDimensions, ProxyRequestOptions, PuppeteerOptions, ServerConfi
 // import from utils
 import { isValidURL } from './utils/isValidURL';
 import { getHostPortSSL } from './utils/getHostPortSSL';
+import { isURLAbsolute, getCorrectHref } from './utils/isURLAbsolute';
 
 // import raw from assets
 // @ts-ignore
@@ -125,9 +127,33 @@ const createServer = ({
         } else {
           logger.info(`********** NEW REQUEST: ${validUrl}`)
 
+          const pageDimensions: PageDimensions = await page.evaluate(() => {
+            let sum = 0;
+            // for some web pages, <html> and <body> have height: 100%
+            // sum up the <body> children's height for an accurate page height
+            document.body.childNodes.forEach((el: Element) => {
+              if (el.nodeType == Node.ELEMENT_NODE) {
+                const style = window.getComputedStyle(el);
+                // filter hidden/collapsible elements 
+                if (style.display == 'none' || style.visibility == 'hidden' || style.opacity == '0') {
+                  return;
+                }
+                // some elements have undefined clientHeight
+                // favor scrollHeight since clientHeight does not include padding
+                if (!isNaN(el.scrollHeight) && !isNaN(el.clientHeight))
+                  sum += el.scrollHeight || el.clientHeight;
+              }
+            });
+            return {
+              width: document.body.scrollWidth || document.body.clientWidth || 1440,
+              height: sum,
+            };
+          });
+
           // cookie will only be set when res is sent succesfully
           const oneHour: number = 1000 * 60 * 60;
           res.cookie('pdftron_proxy_sid', validUrl, { ...COOKIE_SETTING, maxAge: oneHour });
+          res.cookie('pdftron_proxy_height', pageDimensions.height, { ...COOKIE_SETTING, maxAge: oneHour });
           res.status(200).send({ validUrl });
         }
       } catch (err) {
@@ -194,6 +220,7 @@ const createServer = ({
   // // TAKEN FROM: https://stackoverflow.com/a/63602976
   app.use('/', (clientRequest: Request, clientResponse: Response) => {
     const cookiesUrl: string = `${clientRequest.cookies.pdftron_proxy_sid}`;
+    const pageheight: number = clientRequest.cookies.pdftron_proxy_height;
     // check again for all requests that go through the proxy server
     if (cookiesUrl && isValidURL(cookiesUrl, ALLOW_HTTP_PROXY)) {
       const {
@@ -233,46 +260,143 @@ const createServer = ({
         serverResponse.headers['cache-control'] = 'max-age=0, public, no-cache, no-store, must-revalidate';
         let body: string = '';
         // Send html content from the proxied url to the browser so that it can spawn new requests.
-        if (String(serverResponse.headers['content-type']).indexOf('text/html') !== -1) {
-          serverResponse.on('data', (chunk: string) => {
+        const serverResponseContentType = serverResponse.headers['content-type'];
+        if (String(serverResponseContentType).indexOf('text/html') !== -1) {
+          serverResponse.on('data', function (chunk) {
             body += chunk;
           });
 
           serverResponse.on('end', () => {
+            const virtualDOM = new JSDOM(body);
+            const { window } = virtualDOM;
+            const { document } = window;
+            document.documentElement.style.setProperty('--vh', `${pageheight * 0.01}px`);
+
+            document.querySelectorAll("link").forEach(el => {
+              const href = el.getAttribute('href');
+              // filter only CSS links
+              if (el.rel == "stylesheet" || el.type == "text/css" || href.endsWith('.css')) {
+                if (!el.dataset.pdftron && isURLAbsolute(href)) {
+                  // set this attibute to identify if <link> href has been modified
+                  el.setAttribute('data-href', href);
+
+                  const absoluteHref = getCorrectHref(href);
+                  try {
+                    const { hostname, pathname, href, origin } = new URL(absoluteHref);
+                    const hrefWithoutOrigin = href.split(origin)[1] || '';
+                    el.setAttribute('data-domain', hostname);
+                    // check if same domain with cookiesUrl
+                    if (hostname === parsedHost) {
+                      el.setAttribute('data-pdftron', 'same-domain');
+                      el.setAttribute('href', hrefWithoutOrigin);
+                    } else {
+                      // external URLs
+                      // try on https://gotoadvantage.com/ 
+                      el.setAttribute('data-pdftron', 'different-domain');
+                      // el.setAttribute('href', hrefWithoutOrigin);
+
+                      // const {
+                      //   parsedPort: parsedPort1,
+                      //   parsedSSL: parsedSSL1,
+                      // } = getHostPortSSL(absoluteHref);
+                      // console.log('different domain', absoluteHref, parsedPort1, parsedSSL1)
+
+                      // const options1: ProxyRequestOptions = {
+                      //   hostname: hostname,
+                      //   port: parsedPort1,
+                      //   path: `${PATH}${hrefWithoutOrigin}`,
+                      //   method: clientRequest.method,
+                      //   insecureHTTPParser: true,
+                      //   rejectUnauthorized: true, // verify the server's identity
+                      //   headers: {
+                      //     'User-Agent': clientRequest.headers['user-agent'],
+                      //     'Referer': `${PATH}${hrefWithoutOrigin}`,
+                      //     'Accept-Encoding': 'identity', // for amazon to work
+                      //   }
+                      // };
+
+                      // const serverRequest1: ClientRequest = parsedSSL1.request(options1, serverResponse1 => {
+                      //   // callback(serverResponse, clientResponse);
+                      //   let cssContent = '';
+                      //   serverResponse1.on('data', (chunk: string) => cssContent += chunk);
+                      //   serverResponse1.on('end', () => {
+                      //     clientResponse.writeHead(serverResponse1.statusCode, serverResponse1.headers).end(cssContent);
+                      //   });
+                      //   serverResponse1.on('error', (e) => {
+                      //     logger.error(`Http request timeout, ${e}`);
+                      //   });
+                      // });
+
+                      // serverRequest1.on('error', (e) => {
+                      //   serverRequest1.end();
+                      //   logger.error(`Http request, ${e}`);
+                      // });
+                      // serverRequest1.end();
+                    }
+                  } catch (e) {
+                    logger.error(e)
+                  }
+                }
+              }
+            });
+
+            let newBody = virtualDOM.serialize();
+            // let newBody = body;
+
             const styleTag = `<style type='text/css' id='pdftron-css'>${blockNavigationStyle}</style>`;
             const globalVarsScript = `<script type='text/javascript' id='pdftron-js'>window.PDFTron = {}; window.PDFTron.urlToProxy = '${cookiesUrl}';</script>`;
             const debounceScript = `<script type='text/javascript'>${debounceJS}</script>`;
             const navigationScript = `<script type='text/javascript'>${blockNavigationScript}</script>`;
             const textScript = `<script type='text/javascript'>${sendTextDataScript}</script>`;
 
-            const headIndex: number = body.indexOf('</head>');
+            const headIndex: number = newBody.indexOf('</head>');
             if (headIndex > 0) {
-              if (!/pdftron-css/.test(body)) {
-                body = body.slice(0, headIndex) + styleTag + body.slice(headIndex);
+              if (!/pdftron-css/.test(newBody)) {
+                newBody = newBody.slice(0, headIndex) + styleTag + newBody.slice(headIndex);
               }
 
-              if (!/pdftron-js/.test(body)) {
+              if (!/pdftron-js/.test(newBody)) {
                 // order: declare global var first, then debounce, then blocknavigation (switching all href) then send text/link data since the latter happens over and over again
-                body = body.slice(0, headIndex) + globalVarsScript + debounceScript + navigationScript + textScript + body.slice(headIndex);
+                newBody = newBody.slice(0, headIndex) + globalVarsScript + debounceScript + navigationScript + textScript + newBody.slice(headIndex);
               }
             }
 
             delete serverResponse.headers['content-length'];
             clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers);
-            clientResponse.end(body);
+            clientResponse.end(newBody);
           });
 
           serverResponse.on('error', (e) => {
             logger.error(e);
           });
+
+        } else if (String(serverResponseContentType).indexOf('text/css') !== -1) {
+          console.log('serverResponse', (serverResponse as any).req.path)
+          let cssContent = '';
+          serverResponse.on('data', (chunk: string) => cssContent += chunk);
+
+          serverResponse.on('end', () => {
+            // cssContent += `\nh1, h2 {background-color: red !important;}`;
+            cssContent = cssContent.replace(/((height:).{0,10}[\d\s\)]?)vh/g, '$1 * var(--vh)');
+            // write will only append to existing clientResponse and needed to be piped
+            // use writeHead and end for http response
+            // use send for express response
+            clientResponse.writeHead(serverResponse.statusCode, serverResponse.headers).end(cssContent);
+            // clientResponse.set(serverResponse.headers).send(cssContent);
+          });
+
+          serverResponse.on('error', (e) => {
+            logger.error(`Http request timeout, ${e}`);
+          });
+
         } else {
           // Pipe the server response from the proxied url to the browser so that new requests can be spawned for non-html content (js/css/json etc.)
           serverResponse.pipe(clientResponse, {
             end: true,
           });
           // Can be undefined
-          if (serverResponse.headers['content-type']) {
-            clientResponse.contentType(serverResponse.headers['content-type'])
+          if (serverResponseContentType) {
+            clientResponse.contentType(serverResponseContentType)
           }
         }
       }
